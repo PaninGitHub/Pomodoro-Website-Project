0c7fb2ea8a18f7309ac6bc0c7b11290071988d4b
PaninGitHub <diniduk@outlook.com>
2024-07-06 14:56:39 -0500

    Started on input function for timer

--- timer.js
+++ timer.js
@@ -1,8 +1,10 @@
 //Sets constants for calling elements in the HTML
-const disMin = document.getElementById("min");
-const disSec = document.getElementById("sec");
+const disMin = document.getElementById("min-dis");
+const disSec = document.getElementById("sec-dis");
 const button = document.getElementById("timebutton")
 const statustext = document.getElementById("statustext")
+const clockdisplay = document.getElementById("time-display")
+const clockinput = document.getElementById("time-input")
 
 //Audio
 var audio = new Audio('https://cdn.discordapp.com/attachments/1094846471699976263/1258240181757022239/clock-alarm-8761.mp3?ex=668752fc&is=6686017c&hm=e8747124324b47b518782a6208cee22756a74b8d84f3413aff77e953b149d439&')
@@ -49,10 +51,9 @@ function runTimer(){
     }, 1000)
 }
 
-//What should be updated every second
+//Event Handlers
 displayTime()
 button.addEventListener("click", function(){
-    //If the button says "Start" when clicked
     if (isRunning == false){
         button.innerHTML = "Pause";
         isRunning = true;
@@ -65,3 +66,25 @@ button.addEventListener("click", function(){
     }
 })
 
+//Changing time in clock
+clockdisplay.addEventListener("click", function(){
+    clockdisplay.classList.remove("time-display-show")
+    clockdisplay.classList.add("time-display-hide")
+    clockinput.classList.remove("time-input-hide")
+    clockinput.classList.add("time-input-show")
+    //If the clock was running when clicked, it will pause it
+    if (isRunning == true){
+        isRunning = false; 
+        clearInterval(thisInterval);
+    }
+    button.innerHTML = "Enter";
+})
+
+//Enforces a 2-digit limit when changing time in the clock
+for(sel of document.getElementsByClassName("Tin")){
+    sel.addEventListener('input', function(){
+        //The line below take the whole string, selects every non-number, and replaces it with an empty string. Love you ChatGPT for explaining it :))
+        this.value = this.value.replace(/\D/g, '');
+        this.value = this.value.slice(0, 2);
+})
+}
